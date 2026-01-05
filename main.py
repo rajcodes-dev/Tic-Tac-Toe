abc6e60454b6d392bfe138684ec11b32d6ea4079
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-05 15:20:45 +0530

    improved code

--- main.py
+++ main.py
@@ -17,60 +17,73 @@ player_2 = 'O'
 count = 0
 
 while True:
+    
     p = int(input("Enter the position of player - 1(1 to 9): "))
-    p2 = int(input("Enter the position of player - 2(1 to 9): "))
 
     if p in already:
         continue
-    if p2 in already:
-        continue
 
     already.append(p)
-    already.append(p2)
 
     moves[p-1] = player_1
-    moves[p2-1] = player_2
 
-    variable = f"{moves[0]}  | {moves[1]}  | {moves[2]} \n\
+    variable = f"   {moves[0]}  | {moves[1]} | {moves[2]} \n\
     _ | _ | _ \n\
     {moves[3]} | {moves[4]} | {moves[5]} \n\
     _ | _ | _ \n\
     {moves[6]} | {moves[7]} | {moves[8]}\n"
 
-    if (moves[0] and moves[1] and moves[2]) == 'X' or\
-        (moves[3] and moves[4] and moves[5]) == 'X' or\
-        (moves[6] and moves[7] and moves[8]) == 'X':
+    if moves[0] == 'X' and moves[1] == 'X' and moves[2] == 'X' or\
+        moves[3] == 'X' and moves[4] == 'X' and moves[5] == 'X' or\
+        moves[6] == 'X' and moves[7] == 'X' and moves[8] == 'X':
         print(variable)
-        print("You won! 1")
+        print("Player-1 won!")
         break
 
-    if (moves[0] and moves[1] and moves[2]) == 'O' or\
-        (moves[3] and moves[4] and moves[5]) == 'O' or\
-        (moves[6] and moves[7] and moves[8]) == 'O':
+    if moves[0] == 'X' and moves[3] == 'X' and moves[6] == 'X' or\
+        moves[1] == 'X' and moves[4] == 'X' and moves[7] == 'X' or\
+        moves[2] == 'X' and moves[5] == 'X' and moves[8] == 'X':
         print(variable)
-        print("You won! 2")
+        print("Player-1 won!")
         break
 
-    if (moves[0] and moves[3] and moves[6]) == 'O' or\
-        (moves[1] and moves[4] and moves[7]) == 'O' or\
-        (moves[2] and moves[5] and moves[8]) == 'O':
+    if (moves[0] == 'X' and moves[4] == 'X' and moves[8] == 'X') or\
+        (moves[2] == 'X' and moves[4] == 'X' and moves[6] == 'X'):
         print(variable)
-        print("You won! 3")
+        print("Player-1 won!")
         break
 
-    if (moves[0] and moves[3] and moves[6]) == 'X' or\
-        (moves[1] and moves[4] and moves[7]) == 'X' or\
-        (moves[2] and moves[5] and moves[8]) == 'X':
+    print(variable)
+    p2 = int(input("Enter the position of player - 2(1 to 9): "))
+    if p2 in already:
+        continue
+    already.append(p2)
+    moves[p2-1] = player_2
+
+    variable = f"   {moves[0]}  | {moves[1]} | {moves[2]} \n\
+    _ | _ | _ \n\
+    {moves[3]} | {moves[4]} | {moves[5]} \n\
+    _ | _ | _ \n\
+    {moves[6]} | {moves[7]} | {moves[8]}\n"
+
+    if moves[0] == 'O' and moves[1] == 'O' and moves[2] == 'O' or\
+        moves[3] == 'O' and moves[4] == 'O' and moves[5] == 'O' or\
+        moves[6] == 'O' and moves[7] == 'O' and moves[8] == 'O':
         print(variable)
-        print("You won! 4")
+        print("Player-2 won!")
         break
 
-    if (moves[0] and moves[4] and moves[8]) == 'X' or\
-        (moves[2] and moves[4] and moves[6]) == 'X' or\
-        (moves[0] and moves[4] and moves[8]) == 'O' or\
-        (moves[2] and moves[4] and moves[6]) == 'O':
+    if moves[0] == 'O' and moves[3] == 'O' and moves[6] == 'O' or\
+        moves[1] == 'O' and moves[4] == 'O' and moves[7] == 'O' or\
+        moves[2] == 'O' and moves[5] == 'O' and moves[8] == 'O':
         print(variable)
-        print("You won! 5")
+        print("Player-2 won!")
+        break
+
+    if (moves[0] == 'O' and moves[4] == 'O' and moves[8] == 'O') or\
+        (moves[2] == 'O' and moves[4] == 'O' and moves[6] == 'O'):
+        print(variable)
+        print("Player-2 won!")
         break
 
     print(variable)
