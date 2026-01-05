80109169a5dec1e577c12ff40271e96fe0252f6b
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-05 10:49:08 +0530

    added win

--- main.py
+++ main.py
@@ -2,21 +2,31 @@ moves = ['','','',
          '','','',
          '','','']
 
-variable = f"{moves[0]}  | {moves[1]}  | {moves[2]} \n\
+already = []
+
+variable = f"1 | 2 | 3 \n\
 _ | _ | _ \n\
-{moves[3]}  | {moves[4]}  | {moves[5]} \n\
+4 | 5 | 6 \n\
 _ | _ | _ \n\
-{moves[6]}  | {moves[7]}  | {moves[8]} \n"
+7 | 8 | 9 \n"
 
 print(variable)
 
+player_1 = 'X'
+player_2 = 'O'
 count = 0
 
 while True:
-    player_1 = input("Enter the move (player-1): ")
     p = int(input("Enter the position of player - 1(1 to 9): "))
-    player_2 = input("Enter the move (player-2): ")
     p2 = int(input("Enter the position of player - 2(1 to 9): "))
+
+    if p in already:
+        continue
+    if p2 in already:
+        continue
+
+    already.append(p)
+    already.append(p2)
 
     moves[p-1] = player_1
     moves[p2-1] = player_2
@@ -26,6 +36,42 @@ while True:
     {moves[3]} | {moves[4]} | {moves[5]} \n\
     _ | _ | _ \n\
     {moves[6]} | {moves[7]} | {moves[8]}\n"
+
+    if (moves[0] and moves[1] and moves[2]) == 'X' or\
+        (moves[3] and moves[4] and moves[5]) == 'X' or\
+        (moves[6] and moves[7] and moves[8]) == 'X':
+        print(variable)
+        print("You won! 1")
+        break
+
+    if (moves[0] and moves[1] and moves[2]) == 'O' or\
+        (moves[3] and moves[4] and moves[5]) == 'O' or\
+        (moves[6] and moves[7] and moves[8]) == 'O':
+        print(variable)
+        print("You won! 2")
+        break
+
+    if (moves[0] and moves[3] and moves[6]) == 'O' or\
+        (moves[1] and moves[4] and moves[7]) == 'O' or\
+        (moves[2] and moves[5] and moves[8]) == 'O':
+        print(variable)
+        print("You won! 3")
+        break
+
+    if (moves[0] and moves[3] and moves[6]) == 'X' or\
+        (moves[1] and moves[4] and moves[7]) == 'X' or\
+        (moves[2] and moves[5] and moves[8]) == 'X':
+        print(variable)
+        print("You won! 4")
+        break
+
+    if (moves[0] and moves[4] and moves[8]) == 'X' or\
+        (moves[2] and moves[4] and moves[6]) == 'X' or\
+        (moves[0] and moves[4] and moves[8]) == 'O' or\
+        (moves[2] and moves[4] and moves[6]) == 'O':
+        print(variable)
+        print("You won! 5")
+        break
 
     print(variable)
     count += 1
